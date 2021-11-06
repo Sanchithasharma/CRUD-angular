2d8281dbca81bb9fd411a725c58209c9460747d7
sanchithasr <sanchitha.s.r@gmail.com>
2021-11-06 20:02:51 +0530

    added logic to CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './crud/create/create.component';
+import { DisplayComponent } from './crud/create/display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: '', redirectTo: '/create', pathMatch: 'full'},
+  {
+    path: 'display',
+    component: DisplayComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/crud/create/create.component.ts
+++ src/app/crud/create/create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PeopleService } from 'src/app/people.service';
+import {Router} from '@angular/router';
+
 
 
 @Component({
@@ -8,9 +11,24 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./create.component.css']
 })
 
-export class CreateComponent implements OnInit {
-  name = new FormControl('');
-  constructor() { }
+export class CreateComponent implements OnInit { 
+  profileForm = new FormGroup({
+    first_name: new FormControl('', Validators.required),
+    last_name: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone_number: new FormControl('', Validators.required),
+  });
+  constructor(private peopleService: PeopleService, private router: Router) { }
+  
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+    this.peopleService.people.push(this.profileForm.value)
+    console.table(this.peopleService.people)
+    // this.router.navigate(['/home']);
+
+  }
 
   ngOnInit(): void {
   }
